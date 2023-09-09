d2842be4b5c10025a7343e664110bd603a327b76
nikobalek <nikobalek1381@gmail.com>
2023-09-09 03:21:45 +0000

    added letter 'v' and fixed bugs

--- code/num.py
+++ code/num.py
@@ -1,19 +1,23 @@
 uInput = input("Enter your name: ")
 uinput = uInput.lower()
-list = ['0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','w','x','y','z']
+list = ['0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 numList = []
-
-count2 = 0
 
 for count in uInput:
 
-    if list.index(count) <= 9:
+    if count == ' ':
+        continue
+
+    elif list.index(count) <= 9:
         numList.append(list.index(count))
 
     elif list.index(count) > 9 and list.index(count) <= 18:
         numList.append(list.index(count)-9)
 
     elif list.index(count) > 18:
-        numList.append(list.index(count)-17)
+        numList.append(list.index(count)-18)
 
-print(numList)    
+for count in numList:
+    print(count,end=' ')
+
+print()
